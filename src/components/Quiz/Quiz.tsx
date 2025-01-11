5caf087276fc26e08f246c10dbc76b60357bf8aa
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-01-11 21:54:57 +0300

    added check answer logic and progress traker

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,22 +1,69 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { questions } from "../../data/questions";
 
 const Quiz = () => {
+  const [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState(questions[index]);
+  const [lock, setLock] = useState(false);
+  const [correct, setCorrect] = useState(0);
+
+  const checkAnswer = (e, index) => {
+    // console.log(index);
+    if (lock === false) {
+      if (question.answer === index) {
+        e.target.classList.add("bg-green-400");
+        setLock(true);
+      } else {
+        e.target.classList.add("bg-red-400");
+        setLock(true);
+      }
+    }
+  };
+
+  const handleNextQuestion = () => {
+    if (index < questions.length - 1) {
+      if (lock) {
+        setIndex((index) => index + 1);
+        setQuestion(questions[index]);
+        setLock(false);
+      }
+    } else {
+      console.log("end");
+    }
+  };
+
   return (
     <div className="container w-full max-w-2xl mx-auto p-10">
-      <h1 className="text-2xl font-semibold mb-5"> Computer science knowledge check</h1>
+      <h1 className="text-2xl font-semibold mb-5"> Memory and storage </h1>
 
       <div className=" p-3 ">
-        <h3 className="text-lg font-normal mb-4">What stands for HTML</h3>
+        <h3 className="text-md font-normal mb-4">
+          {" "}
+          <span className="text-bold h-2 w-16 p-2 mr-3 bg-black text-white">{index + 1}</span>
+          {question.question}
+        </h3>
+
         <ul className="flex flex-col space-y-3">
-          <li className="text-sm p-3 border border-slate-300 rounded-md cursor-pointer">Hypertext Markup Language</li>
-          <li className="text-sm p-3 border border-slate-300 rounded-md cursor-pointer">B :Hypertext Markup Language</li>
-          <li className="text-sm p-3 border border-slate-300 rounded-md cursor-pointer">C :Hypertext Markup Language</li>
-          <li className="text-sm p-3 border border-slate-300 rounded-md cursor-pointer">D :Hypertext Markup Language</li>
+          {question.options.map((option, index) => (
+            <li
+              className={`text-sm p-3 border border-slate-300 rounded-md cursor-pointer`}
+              key={index}
+              onClick={(e) => {
+                checkAnswer(e, index);
+              }}
+            >
+              {option}
+            </li>
+          ))}
         </ul>
         <div className="py-3 flex items-center justify-end">
-          <button className="px-12 py-2 rounded-md bg-[#2dcca5]">Next</button>
+          <button className="px-12 py-2 rounded-md bg-[#2dcca5]" onClick={handleNextQuestion}>
+            Next
+          </button>
         </div>
-        <div>1 out of 5</div>
+        <div>
+          {index + 1} out of {questions.length}
+        </div>
       </div>
     </div>
   );
---
+++ src/data/questions.ts
@@ -0,0 +1,43 @@
+export const questions = [
+  {
+    question: "What is primary memory?",
+    options: [
+      "An internal component which holds data and programs that are not currently in use",
+      "An external component which holds data and programs that are currently in use",
+      "An internal component which holds data and programs that are currently in use",
+    ],
+    answer: 2,
+  },
+  {
+    question: "What are the two types of primary memory?",
+    options: ["RAM and ROM", "Flash memory and cache memory", "Flash memory and virtual memory"],
+    answer: 0,
+  },
+  {
+    question: "In terms of memory, what does volatile mean?",
+    options: [
+      "The memory retains its contents when the power is removed",
+      "The memory can be overwritten",
+      "The memory loses its contents when the power is removed",
+    ],
+    answer: 2,
+  },
+  {
+    question: "Which of these statements about ROM is true?",
+    options: [
+      "ROM is non-volatile and can only be read from",
+      "ROM is volatile and can only be read from",
+      "ROM is non-volatile and can be read from and written to",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Which of these statements about RAM is true?",
+    options: [
+      "RAM is non-volatile and can only be read from",
+      "RAM is volatile and can only be read from",
+      "RAM is volatile and can be read from and written to",
+    ],
+    answer: 1,
+  },
+];
